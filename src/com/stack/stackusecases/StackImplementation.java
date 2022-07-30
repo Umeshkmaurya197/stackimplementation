067ee0884b76b7880f96246f22afb4b02c745f1b
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-30 15:25:09 +0530

    Refactored : added getSize() method

--- src/com/stack/stackusecases/StackImplementation.java
+++ src/com/stack/stackusecases/StackImplementation.java
@@ -3,10 +3,16 @@ package com.stack.stackusecases;
 public class StackImplementation<T> {
 
 	public Node<T> head;
+	public int size;
+	
+	public StackImplementation() {
+		this.size=0;
+	}
 
 	// push -uc1
 	public void push(T data) {
 		Node<T> newNode = new Node<>(data);
+		size++;
 		if (head == null) {
 			head = newNode;
 			return;
@@ -21,6 +27,7 @@ public class StackImplementation<T> {
 			System.out.println(" Stack is empty .");
 			return null;
 		}
+		size--;
 		Node<T> top=head;
 		head = head.next;
 		return top;
@@ -50,6 +57,10 @@ public class StackImplementation<T> {
 		System.out.println(" ");
 	}
 
+	public int getSize() {
+		return size;
+	}
+
 	public static void main(String[] args) {
 		StackImplementation<Integer> stackImplementation = new StackImplementation<>();
 		stackImplementation.push(70);
@@ -61,5 +72,6 @@ public class StackImplementation<T> {
 		stackImplementation.printStack(); // 30 -> 70 ->
 
 		System.out.println("Peek element is : " + stackImplementation.peek()); // 30
+		System.out.println("Stack size : " + stackImplementation.getSize());
 	}
 }
